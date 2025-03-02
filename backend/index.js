15553f360807ec5af21290d938185272ad139bd4
NeilHundia <hundianeil@gmail.com>
2025-03-02 23:04:53 +0530

    Database Corrections

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect('mongodb+srv://neilhundia:Qnut12RM4uspQWMi@collabro.qqtgd.mongodb.net/').then(()=>{
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL).then(()=>{
     console.log("Connected to Database");
 }).catch((err)=>{
     console.error(err);
 })
 
 const app = express();
+app.use(express.json());
 const PORT = 8000;
 app.listen(PORT,()=>{
     console.log(`Server is listening at port:${PORT}`);
