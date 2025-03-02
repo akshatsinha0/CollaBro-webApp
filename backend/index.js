d91de92858444755d163e5d70bbaf1db08ccf11e
NeilHundia <hundianeil@gmail.com>
2025-03-02 22:56:51 +0530

    Connected Database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://neilhundia:Qnut12RM4uspQWMi@collabro.qqtgd.mongodb.net/').then(()=>{
+    console.log("Connected to Database");
+}).catch((err)=>{
+    console.error(err);
+})
+
 const app = express();
 const PORT = 8000;
 app.listen(PORT,()=>{
     console.log(`Server is listening at port:${PORT}`);
 })
+
